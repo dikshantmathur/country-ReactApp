6092bb8e60e324edca1219ec6a96194d60617f01
dikshantmathur <dikshant.17jice038@jietjodhpur.ac.in>
2020-07-21 23:59:49 +0530

    Updates

---
+++ precache-manifest.7a12c7c4a26a5d1e6e95a51cde5fe3eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a362b1be348bd2e5f95d2c7bfc1a3e",
+    "url": "/country-ReactApp/index.html"
+  },
+  {
+    "revision": "e0b956a188bb12ed9c17",
+    "url": "/country-ReactApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f087caf52373b326a951",
+    "url": "/country-ReactApp/static/css/main.3b064ea0.chunk.css"
+  },
+  {
+    "revision": "e0b956a188bb12ed9c17",
+    "url": "/country-ReactApp/static/js/2.e68edcbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/country-ReactApp/static/js/2.e68edcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f087caf52373b326a951",
+    "url": "/country-ReactApp/static/js/main.4548419a.chunk.js"
+  },
+  {
+    "revision": "ffeebc3c49a6125ed692",
+    "url": "/country-ReactApp/static/js/runtime-main.0038c758.js"
+  },
+  {
+    "revision": "14c89a41e5154b4a2920df0115337ad5",
+    "url": "/country-ReactApp/static/media/react-logo.14c89a41.png"
+  }
+]);
